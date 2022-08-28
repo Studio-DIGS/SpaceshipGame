b99fb775ed28203ec8715c712bf56bb2dd4249f7
Raiyan <rsahmed1290@gmail.com>
2022-08-28 14:17:39 -0700

    added bullet trails

--- Assets/_Scripts/DespawnTimer.cs
+++ Assets/_Scripts/DespawnTimer.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DespawnTimer : MonoBehaviour
 {
     [SerializeField] float time;
+    public ParticleSystem despawnParticles;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, time);
+
     }
 
 }
